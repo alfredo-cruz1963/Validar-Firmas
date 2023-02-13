15b50edb462a523b584461dc539157a630cfb989
Alfredo <alfredogutierrezcruzu@hotmail.com>
2023-02-13 17:17:20 -0500

    config corregido

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.PASSWORD || 'root',
     database: process.env.DATABASE || 'db_firmas',
     key: process.env.KEY || 'e6ab3e025abbb075676a18fd3e23168ffffa1cdc60749f4b3569c',
-    client: process.env.CLIENT || 484,
+    client: process.env.CLIENT || 484
 }
 
  
